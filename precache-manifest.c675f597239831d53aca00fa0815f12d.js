41aaf7329ea5fe02c987cb25502c664f4b711e41
unknown <miihauu@gmail.com>
2019-04-16 20:47:31 +0200

    Updates

---
+++ precache-manifest.c675f597239831d53aca00fa0815f12d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cfec8f0523474c56ca3",
+    "url": "/accordion-buttons/static/js/runtime~main.65a93436.js"
+  },
+  {
+    "revision": "0585d3fc891c8a1f78cd",
+    "url": "/accordion-buttons/static/js/main.efc238cb.chunk.js"
+  },
+  {
+    "revision": "1d2e6a5b3b5cd041116c",
+    "url": "/accordion-buttons/static/js/2.458c05cb.chunk.js"
+  },
+  {
+    "revision": "0585d3fc891c8a1f78cd",
+    "url": "/accordion-buttons/static/css/main.ea8f21d0.chunk.css"
+  },
+  {
+    "revision": "84dfdacba6b25387483e6fa1ba6f7a96",
+    "url": "/accordion-buttons/index.html"
+  }
+];
